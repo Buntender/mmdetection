0b50c0af0064ff48b08ce363ef684a5df40ba051
makinokatsumi <liukezhao@stu.xjtu.edu.cn>
2022-11-17 21:12:09 +0800

    voc robust test

---
+++ robustdetector/apis/robusttest.py
@@ -0,0 +1,96 @@
+# Copyright (c) OpenMMLab. All rights reserved.
+import os.path as osp
+import pickle
+import shutil
+import tempfile
+import time
+
+import mmcv
+import torch
+import torch.distributed as dist
+from mmcv.image import tensor2imgs
+from mmcv.runner import get_dist_info
+
+from mmdet.core import encode_mask_results
+
+
+def robust_single_gpu_test(model,
+                    data_loader,
+                    show=False,
+                    out_dir=None,
+                    show_score_thr=0.3):
+    model.eval()
+    results = []
+    dataset = data_loader.dataset
+    PALETTE = getattr(dataset, 'PALETTE', None)
+    prog_bar = mmcv.ProgressBar(len(dataset))
+    for i, data in enumerate(data_loader):
+        gts = data_loader.dataset.get_ann_info(i)
+        perturb = data['img'][0].new(data['img'][0].size()).uniform_(-1, 1)
+        ori = data['img'][0]
+        for r in range(10):
+            data['img'][0] = ori + perturb
+            data['img'][0].detach_()
+            data['img'][0].requires_grad_()
+            loss = model(img=data['img'][0], img_metas=data['img_metas'][0], gt_bboxes=[torch.tensor(gts['bboxes'])], gt_labels=[torch.tensor(gts['labels'])])
+            loss = model.module._parse_losses(loss)
+            loss[0].backward()
+
+            grad = data['img'][0].grad.clone()
+            perturb += grad.sign()
+            perturb = perturb.clamp(-8, 8)
+            perturb.detach_()
+
+        with torch.no_grad():
+            # datarefine = {'img': [data['img'].data[0]], 'img_metas': [data['img_metas'].data[0]]}
+            # result = model(return_loss=False, rescale=True, **datarefine)
+            result = model(return_loss=False, rescale=True, **data)
+
+        batch_size = len(result)
+        if show or out_dir:
+            if batch_size == 1 and isinstance(data['img'][0], torch.Tensor):
+                img_tensor = data['img'][0]
+            else:
+                img_tensor = data['img'][0].data[0]
+            img_metas = data['img_metas'][0].data[0]
+            imgs = tensor2imgs(img_tensor, **img_metas[0]['img_norm_cfg'])
+            assert len(imgs) == len(img_metas)
+
+            for i, (img, img_meta) in enumerate(zip(imgs, img_metas)):
+                h, w, _ = img_meta['img_shape']
+                img_show = img[:h, :w, :]
+
+                ori_h, ori_w = img_meta['ori_shape'][:-1]
+                img_show = mmcv.imresize(img_show, (ori_w, ori_h))
+
+                if out_dir:
+                    out_file = osp.join(out_dir, img_meta['ori_filename'])
+                else:
+                    out_file = None
+
+                model.module.show_result(
+                    img_show,
+                    result[i],
+                    bbox_color=PALETTE,
+                    text_color=PALETTE,
+                    mask_color=PALETTE,
+                    show=show,
+                    out_file=out_file,
+                    score_thr=show_score_thr)
+
+        # encode mask results
+        if isinstance(result[0], tuple):
+            result = [(bbox_results, encode_mask_results(mask_results))
+                      for bbox_results, mask_results in result]
+        # This logic is only used in panoptic segmentation test.
+        elif isinstance(result[0], dict) and 'ins_results' in result[0]:
+            for j in range(len(result)):
+                bbox_results, mask_results = result[j]['ins_results']
+                result[j]['ins_results'] = (bbox_results,
+                                            encode_mask_results(mask_results))
+
+        results.extend(result)
+
+        for _ in range(batch_size):
+            prog_bar.update()
+    return results
---
+++ robustdetector/tools/stdtest.py
@@ -0,0 +1,5 @@
+from tools.test import main
+from mmdet.apis import single_gpu_test
+
+if __name__ == '__main__':
+    main(single_gpu_test)
