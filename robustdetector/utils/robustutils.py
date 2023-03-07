f221a2089871b81e4688e5f8bcdb71b80da597cc
makinokatsumi <liukezhao@stu.xjtu.edu.cn>
2023-03-07 14:59:18 +0800

    update TODO

--- robustdetector/utils/robustutils.py
+++ robustdetector/utils/robustutils.py
@@ -2,6 +2,7 @@ import torch
 
 # mean = torch.tensor([123.675, 116.28, 103.53]).view(1, -1, 1, 1)
 
+#TODO cast */std
 def perturbupdater(perturb, grad, ori, norm_cfg):
     mean = torch.tensor(norm_cfg['mean']).view(1, -1, 1, 1).to(ori.device)
     std = torch.tensor(norm_cfg['std']).view(1, -1, 1, 1).to(ori.device)
